e51a9ada11cdb03da6d2ffcacff6bdd136c7c0fa
Viktor <tomahabk59@gmail.com>
2023-01-31 16:45:59 +0500

    creat_model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class SubCategories(models.Model):
+    categories = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+
+class Tools(models.Model):
+    subcatigories = models.ForeignKey(SubCategories, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    description = models.TextField('Описание')
+    picture = models.ImageField(
+        'Фото', upload_to=None, height_field=None, width_field=None, max_length=None)
+    availability = models.BooleanField('Наличие', default=True)
