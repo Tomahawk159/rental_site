0594ecb246a16cdafe859824aef7d5dd62b0dccc
Viktor <tomahabk59@gmail.com>
2023-02-01 12:24:13 +0500

    create_migrate

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mainapp.models import *
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Tool)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,19 +1,32 @@
 from django.db import models
 
 
-class Categories(models.Model):
-    name = models.CharField(max_length=100)
+class Category(models.Model):
+    name = models.CharField('Наименование', max_length=100)
+    picture = models.ImageField(
+        'Фото', upload_to='Images', height_field=None, width_field=None, max_length=None)
+
+    def __str__(self):
+        return self.name
 
 
-class SubCategories(models.Model):
-    categories = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+class SubCategory(models.Model):
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField('Наименование', max_length=100)
+    picture = models.ImageField(
+        'Фото', upload_to='Images', height_field=None, width_field=None, max_length=None)
+
+    def __str__(self):
+        return self.name
 
 
-class Tools(models.Model):
-    subcatigories = models.ForeignKey(SubCategories, on_delete=models.CASCADE)
-    name = models.CharField(max_length=150)
+class Tool(models.Model):
+    subcategories = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    name = models.CharField('Наименование', max_length=150)
     description = models.TextField('Описание')
     picture = models.ImageField(
-        'Фото', upload_to=None, height_field=None, width_field=None, max_length=None)
+        'Фото', upload_to='Images', height_field=None, width_field=None, max_length=None)
     availability = models.BooleanField('Наличие', default=True)
+
+    def __str__(self):
+        return self.name
