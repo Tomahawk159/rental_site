e9054dd36725e7065b4923d52ae3c6a188ab494a
Viktor <tomahabk59@gmail.com>
2023-02-03 14:54:57 +0500

    add static and templates

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from mainapp import views
+from mainapp.apps import MainappConfig
 
 
 urlpatterns = [
-    
+    path('', views.IndexView.as_view()),
+    path('contact/', views.ContactView.as_view()),
+    path('catalog/', views.CatalogView.as_view()),
+    path('about-us/', views.AboutUsView.as_view())
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+from django.views.generic import TemplateView
+from mainapp.models import *
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+
+class CatalogView(TemplateView):
+    template_name = 'mainapp/catalog.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'mainapp/contact.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'mainapp/about_us.html'
