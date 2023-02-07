46dc3982f2087c98c2aee718f5ff1afd1d56adb9
Viktor <tomahabk59@gmail.com>
2023-02-07 08:51:18 +0500

    update_templates

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from mainapp import views
 from mainapp.apps import MainappConfig
 
+app_name = MainappConfig.name
+
 
 urlpatterns = [
     path('', views.IndexView.as_view()),
     path('contact/', views.ContactView.as_view()),
     path('catalog/', views.CatalogView.as_view()),
-    path('about-us/', views.AboutUsView.as_view())
+    path('about_us/', views.AboutUsView.as_view())
 ]
