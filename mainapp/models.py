c4e39142b3d8827c0848088ed5041762a0a274d8
Kropochev Aleksandr <kropochev.a@gmail.com>
2023-02-23 14:49:03 +0500

    fix typos

--- mainapp/models.py
+++ mainapp/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 
 
 class SubCategory(models.Model):
-    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField('Наименование', max_length=100)
     picture = models.ImageField(
         'Фото', upload_to='subcategory_image', height_field=None, width_field=None, max_length=None)
@@ -25,11 +25,11 @@ class SubCategory(models.Model):
         verbose_name_plural = 'подкатегории'
 
     def __str__(self):
-        return f'{self.name} | {self.categories}'
+        return f'{self.name} | {self.category}'
 
 
 class Tool(models.Model):
-    subcategories = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
     name = models.CharField('Наименование', max_length=150)
     description = models.TextField('Описание')
     picture = models.ImageField(
@@ -41,4 +41,4 @@ class Tool(models.Model):
         verbose_name_plural = 'инструменты'
 
     def __str__(self):
-        return f'{self.name} | {self.subcategories}'
+        return f'{self.name} | {self.subcategory}'
