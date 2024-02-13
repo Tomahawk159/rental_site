3a9a90788a0040a1c7c259fd3d33a78b2515cdbc
Lazarev Viktor <tomahabk59@gmail.com>
2024-02-13 13:46:05 +0500

    sending feedback via telegram

--- review/views.py
+++ review/views.py
@@ -2,10 +2,12 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator
+from common.common_variable import TG_CHAT_ID
 
 from common.views import TitleMixin
 from review.models import Review
 from review.forms import ReviewForm
+from telegram.views import send_telegram_message
 
 
 class ReviewView(TitleMixin, SuccessMessageMixin, FormView):
@@ -27,5 +29,12 @@ class ReviewView(TitleMixin, SuccessMessageMixin, FormView):
     def form_valid(self, form):
         name = form.cleaned_data['name']
         review = form.cleaned_data['review']
+        data = {
+                    'chat_id': TG_CHAT_ID,
+                    'text': f"""Оставлен отзыв от: {name}"""
+                            f"""\nТекст отзыва: {review}"""
+                            f"""\nЗайдите в админку,чтоб подтвердить его"""
+                }
+        send_telegram_message(data)
         Review.objects.create(name=name, review=review)
         return super().form_valid(form)
